fd71e7ba2362ebd9368ce32a5fb1b60e1b148875
Pugpaprika21 <pugpaprika21@gmail.com>
2022-10-05 07:10:34 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once('../twig/vendor/autoload.php');
+
+$loader = new FilesystemLoader('../twig/views/');
+
+$twig = new Environment($loader, array(
+    'cache' => '/path/to/compilation_cache',
+));
+
+echo $twig->render('index.html', array(
+
+    'users' => array(
+        array('name' => 'alex', 'age' => 25, 'sex' => 'male'),
+        array('name' => 'joe', 'age' => 18, 'sex' => 'male'),
+        array('name' => 'lisa', 'age' => 22, 'sex' => 'female')
+    )
+
+));
