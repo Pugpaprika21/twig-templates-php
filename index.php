414c2b881e74399da41ca1f834efc26ac7647143
Pugpaprika21 <pugpaprika21@gmail.com>
2022-10-05 07:18:22 -0700

    e

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Twig\Environment;
+use Twig\Lexer;
 use Twig\Loader\FilesystemLoader;
 
 require_once('../twig/vendor/autoload.php');
@@ -11,12 +12,15 @@ $twig = new Environment($loader, array(
     'cache' => '/path/to/compilation_cache',
 ));
 
-echo $twig->render('index.html', array(
+$lexer = new Lexer($twig, array(
+    'tag_block' => ['{', '}'],
+    'tag_variable' => ['{{ $', '}}']
+));
 
+echo $twig->render('index.html', array(
     'users' => array(
         array('name' => 'alex', 'age' => 25, 'sex' => 'male'),
         array('name' => 'joe', 'age' => 18, 'sex' => 'male'),
         array('name' => 'lisa', 'age' => 22, 'sex' => 'female')
     )
-
 ));
