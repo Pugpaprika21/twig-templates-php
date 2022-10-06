b7ee83d697dae4ff92a91d551b02b08c850b8393
Pugpaprika21 <pugpaprika21@gmail.com>
2022-10-06 07:45:00 -0700

    e

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 require_once('../twig/vendor/autoload.php');
 
 $loader = new FilesystemLoader('../twig/views/');
 
-$twig = new Environment($loader, [
-    'cache' => '/path/to/compilation_cache',
-]);
+$twig = new Environment($loader, array(
+    'debug' => true, // {{ dump(array|object) }}
+));
 
-echo $twig->render('index.php', [
-    'name' => 'Fabien',
-    'age' => 24
-]);
+$twig->addExtension(new DebugExtension());
+
+echo $twig->render('index_view.php', array(
+    'name' => 'alex',
+    'age' => 25,
+    'users' => array(
+        array('name' => 'joe', 'age' => 24, 'sex' => 'male'),
+        array('name' => 'jim', 'age' => 18, 'sex' => 'male'),
+        array('name' => 'lisa', 'age' => 26, 'sex' => 'female')
+    )
+));
---
+++ views/index_view.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Twig</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        .container {
+            margin-top: 50px;
+        }
+
+        #user-table {
+            color: #ffffff;
+            background-color: #F1C40F;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <table class="table table-bordered">
+            <thead id="user-table">
+                <tr>
+                    <td>#</td>
+                    <td>name</td>
+                    <td>age</td>
+                    <td>sex</td>
+                    <td>action</td>
+                </tr>
+            </thead>
+            <tbody>
+                {% for user in users %}
+                    <tr>
+                        <td>{{ loop.index }}</td>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.age }}</td>
+                        {% if user.sex == 'male' %}
+                            <td><span class="badge text-bg-success">{{ user.sex }}</span></td>
+                        {% elseif user.sex == 'female' %}
+                            <td><span class="badge text-bg-danger">{{ user.sex }}</span></td>
+                        {% endif %}
+                        <td><a href="views/get.php?id={{ loop.index }}">{{ user.name }}</a></td>
+                    </tr>
+                {% endfor %}
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
